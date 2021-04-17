04dccc6542d740c7eba375854afe1050a9ef8ade
caimiao <caimiao2017@outlook.com>
2021-04-17 14:44:14 +0000

    访问策略

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            session()->flash('info', $request->is('signup') ? '您已注册并已登录！' : '您已登录，无需再次操作。');
+            return redirect('/');
         }
 
         return $next($request);

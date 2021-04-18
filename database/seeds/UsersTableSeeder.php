21ccef73b3c9c177206945a6d2824af5904b11f3
caimiao <caimiao2017@outlook.com>
2021-04-18 05:51:16 +0000

    查看你用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = 'caimiao';
+        $user->email = 'caimiao@outlook.com';
+        $user->password = bcrypt('123456');
+        $user->save();
+    }
+}
